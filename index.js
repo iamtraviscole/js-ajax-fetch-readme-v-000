ea358560f46e30b2480329ed9b1f8d21a7f78afa
Travis Cole <iamtraviscole@gmail.com>
2017-10-31 05:08:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = '8b0abf5aa43311f57b3981a732990aafd99132ca'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
